89c034749ddfa7813ea66298e6ac2017457bb6fe
zhangyunjie <jiesoul@gmail.com>
2019-08-30 11:47:13 +0800

    goon

--- src/cpp-primer/ch02/Sales_data.h
+++ src/cpp-primer/ch02/Sales_data.h
@@ -2,8 +2,8 @@
 #define SALES_DATA_H
 #include <string>
 struct Sales_data {
-  std::string bookNo;
-  unsigned units_sold = 0;
-  double revenue = 0.0;
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
 };
 #endif
