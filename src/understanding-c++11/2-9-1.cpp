15aa59186643457e4b3079b5388c7f9d5b739956
jiesoul <jiesoul@gmail.com>
2019-10-11 19:25:31 +0800

    Merge branch 'master' of /Users/jiesoul/projects/private/c-cpp-learn with conflicts.

---
+++ src/understanding-c++11/2-9-1.cpp
@@ -0,0 +1,18 @@
+//
+// Created by jiesoul on 2019/9/27.
+//
+class Poly;
+typedef Poly P;
+
+class LiLei {
+  friend class Poly;
+};
+
+class Jim {
+  friend Poly;
+};
+
+class HanMeiMei {
+  friend  P;
+};
+
