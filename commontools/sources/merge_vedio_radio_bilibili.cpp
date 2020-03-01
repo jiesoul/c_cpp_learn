6123a512327be9c0547cdba22e9ac6f379f87566
jiesoul <jiesoul@gmail.com>
2020-03-02 01:09:23 +0800

    支持BILIBILI 目录下不同的分辨率自动识别

--- commontools/sources/merge_vedio_radio_bilibili.cpp
+++ commontools/sources/merge_vedio_radio_bilibili.cpp
@@ -2,7 +2,8 @@
  * B站手机缓存下载音频视频是分离，此程序为使用 ffmpeg 合并为一个文件。 
  * Author： jiesoul
  * Date：2020-03-01
- * 
+ * 参数：文件目录
+ * 自动支持 1080p 下分辨率。
  **/
 
 #include <iostream>
@@ -12,6 +13,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+
 int main(int argc, char *argv[])
 {
     std::string input(argv[1]);
@@ -20,13 +22,29 @@ int main(int argc, char *argv[])
         auto const pos = dir_path.find_last_of("/");
         auto filename = dir_path.substr(pos + 1);
         if (filename.size() == 1) filename = "0" + filename;
-        std::cout << filename << std::endl;
         if (dir_entry.is_directory()) {
-            const auto& video = dir_path + "/16/video.m4s";
-            const auto& audio = dir_path + "/16/audio.m4s";
-            std::string c = "ffmpeg -i " + video + " -i " + audio + " -c copy " + input + "/" + filename + ".mp4";
-            std::cout << "begin exec：" << const_cast<char *>(c.c_str()) << std::endl;
-            system(const_cast<char *>(c.c_str()));
+            int p = 16;
+            while(p < 257) {
+                std::filesystem::path new_path(dir_path + "/" + std::to_string(p));
+                if (!std::filesystem::directory_entry(new_path).exists()) {
+                    p *= 2;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            if (p > 256) {
+                    std::cout << "找不到相应的目录" << std::endl;
+                    return -1;
+            } else {
+                const auto& video = dir_path + "/" + std::to_string(p) + "/video.m4s";
+                const auto& audio = dir_path + "/" + std::to_string(p) + "/audio.m4s";
+                std::string c = "ffmpeg -i " + video + " -i " + audio + " -c copy " + input + "/" + filename + ".mp4";
+                std::cout << "begin exec：" << const_cast<char *>(c.c_str()) << std::endl;
+                system(const_cast<char *>(c.c_str()));
+            }
+        } else {
+            continue;
         }
     }
     return 1;
